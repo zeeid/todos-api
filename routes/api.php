1ebe2f1e92b7458949f999e8c991c21fc32edf01
DESKTOP-4QHJ5NU\Zasrok <zeeideveloper19@gmail.com>
2025-11-27 14:34:16 +0700

    update route api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+});
