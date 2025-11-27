9b9c5c57990f3139506b9550f7c6c49d4dab5a3c
DESKTOP-4QHJ5NU\Zasrok <zeeideveloper19@gmail.com>
2025-11-27 13:29:00 +0700

    implement model user to auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -11,13 +11,22 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        // echo json_encode($request);die();
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => Hash::make($request->password)
         ]);
 
-        return response()->json($user);
+        return response()->json([
+            'message' => 'User registered successfully'
+        ], 201);
     }
 
     public function login(Request $request)
@@ -25,9 +34,16 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (! $token = JWTAuth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Invalid Credentials'], 401);
         }
 
-        return response()->json(compact('token'));
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    public function me()
+    {
+        return response()->json(JWTAuth::user());
     }
 }
