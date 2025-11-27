9f8106474afda893660f75b94b1337aef7396647
DESKTOP-4QHJ5NU\Zasrok <zeeideveloper19@gmail.com>
2025-11-27 13:06:43 +0700

    create todo models

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_done',
+        'user_id'
+    ];
 }
