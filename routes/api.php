310fe65d360ffdc6b4b4146e16b06372c8c257dd
DESKTOP-4QHJ5NU\Zasrok <zeeideveloper19@gmail.com>
2025-11-27 15:02:42 +0700

    add api crud todos

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
+
+    Route::get('/todos', [TodoController::class, 'index']);      
+    Route::post('/todos', [TodoController::class, 'store']);     
+    Route::put('/todos/{id}', [TodoController::class, 'update']); 
+    Route::delete('/todos/{id}', [TodoController::class, 'destroy']); 
 });
